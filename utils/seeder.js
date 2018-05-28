52ca588a264bdc93d006185d1cd64f1164cb5b19
Luca Di Gaspero <l.digaspero@uniud.it>
2018-05-28 11:39:09 +0200

    Seeder is more robust now.

--- utils/seeder.js
+++ utils/seeder.js
@@ -4,16 +4,20 @@ import User from './user';
 
 mongoose.connect("mongodb://localhost/rps");
 
-User.collection.drop(); // deletes the users collection
-User.create([{
-    email: "test@example.com",
-    name: "Test Account",
-    password: md5("test")
-}]).then(users => {
-    console.log(`${users.length} users created`);
-}).catch((err) => {
-    console.log(err);
-}).finally(() => {
-    mongoose.connection.close();
-});
-
+mongoose.connection.dropCollection('users')
+    .catch(function() { 
+        // do nothing, probably the collection didn't exist
+    })
+    .finally(function() {
+        User.create([{
+            email: "test@example.com",
+            name: "Test Account",
+            password: md5("test")
+        }]).then(users => {
+            console.log(`${users.length} users created`);
+        }).catch((err) => {
+            console.log(err);
+        }).finally(() => {
+            mongoose.connection.close();
+        });
+    });
